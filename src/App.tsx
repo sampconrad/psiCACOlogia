0368c99d21e1ee6067c30ce8f4d8fba6d7790df3
Conrado Sampaio <102433904+sampconrad@users.noreply.github.com>
2024-11-10 00:18:44 -0300

    feat: added new words to the game

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,20 @@
-import { useState, useEffect } from 'react';
-import { Input, Button, Stack, Flex, Center, Image, Heading, Highlight, Text, Avatar, Container } from '@chakra-ui/react';
-import { FaHeart, FaHeartBroken, FaPen, FaRedoAlt } from 'react-icons/fa';
+import {
+  Avatar,
+  Button,
+  Center,
+  Container,
+  Flex,
+  Heading,
+  Highlight,
+  Image,
+  ImageProps,
+  Input,
+  Stack,
+  Text,
+} from '@chakra-ui/react';
 import { motion } from 'framer-motion';
-import { ImageProps } from '@chakra-ui/react';
-
+import { useEffect, useState } from 'react';
+import { FaHeart, FaHeartBroken, FaPen, FaRedoAlt } from 'react-icons/fa';
 
 import caco1 from './assets/caco1.png';
 import caco3 from './assets/caco3.png';
@@ -11,8 +22,7 @@ import caco4 from './assets/caco4.png';
 import caco5 from './assets/caco5.png';
 import caco6 from './assets/caco6.png';
 import caco7 from './assets/caco7.gif';
-import ingra from './assets/ingra.png'
-
+import ingra from './assets/ingra.png';
 
 interface HeartProps {
   color: string;
@@ -24,28 +34,28 @@ interface WordsToGuessProps {
   tip: string;
 }
 
-const numberOfAttempts = 6
+const numberOfAttempts = 6;
 
 const wordsToGuess = [
   {
-    word: "sistema límbico",
-    tip: "Sistema responsável pelas emoções humanas."
+    word: 'marsha',
+    tip: 'Nome da criadora da DBT.',
   },
   {
-    word: "emoções",
-    tip: "Sentimento subjetivo do ser humano."
+    word: 'borderline',
+    tip: 'Transtorno melhor tratado pela DBT.',
   },
   {
-    word: "papez",
-    tip: "Explicou pela primeira vez as emoções em 1937."
+    word: 'aceitação e mudança',
+    tip: 'Práticas utilizadas pela DBT.',
   },
   {
-    word: "comportamentais",
-    tip: "As emoções podem ser fisiológicas e __________?"
+    word: 'dialética',
+    tip: 'Filosofia em que se baseia a DBT.',
   },
   {
-    word: "amígdala",
-    tip: "Principal componente do cérebro ligado às emoções."
+    word: 'mindfullness',
+    tip: 'Técnica usada para concentração no momento presente.',
   },
 ];
 
@@ -58,17 +68,18 @@ const removeDiacritics = (str: string) => {
   return str.normalize('NFD').replace(/[\u0300-\u036f]/g, '');
 };
 
-const Heart = ({ color, key } : HeartProps) => {
+const Heart = ({ color, key }: HeartProps) => {
   return (
     <motion.div
       key={key}
       initial={{ opacity: 1 }}
-      animate={{ opacity: color === 'red' ? 1 : .4 }}
-      transition={{ duration: 0.5 }}
-    >
-      {color === 'red' 
-        ? <FaHeart style={{color: 'red'}}/>
-        : <FaHeartBroken  style={{color: '#222222'}}/> }
+      animate={{ opacity: color === 'red' ? 1 : 0.4 }}
+      transition={{ duration: 0.5 }}>
+      {color === 'red' ? (
+        <FaHeart style={{ color: 'red' }} />
+      ) : (
+        <FaHeartBroken style={{ color: '#222222' }} />
+      )}
     </motion.div>
   );
 };
@@ -85,9 +96,7 @@ const App = () => {
   const [guess, setGuess] = useState('');
   const [remainingAttempts, setRemainingAttempts] = useState(numberOfAttempts);
   const [correctGuesses, setCorrectGuesses] = useState<string[]>([]);
-  const [wordToGuess, setWordToGuess] = useState<WordsToGuessProps>(
-    getRandomWord()
-  );
+  const [wordToGuess, setWordToGuess] = useState<WordsToGuessProps>(getRandomWord());
   const [gameOver, setGameOver] = useState(false);
 
   useEffect(() => {
@@ -101,21 +110,25 @@ const App = () => {
   const handleGuessSubmit = () => {
     const newCorrectGuesses = [...correctGuesses];
     let newRemainingAttempts = remainingAttempts;
-  
+
     const normalizedWord = removeDiacritics(wordToGuess.word);
-  
+
     if (guess && normalizedWord.includes(guess)) {
       newCorrectGuesses.push(guess);
-      const specialChars = wordToGuess.word.split('').filter((letter) => removeDiacritics(letter) === guess && !newCorrectGuesses.includes(letter));
+      const specialChars = wordToGuess.word
+        .split('')
+        .filter(
+          (letter) => removeDiacritics(letter) === guess && !newCorrectGuesses.includes(letter)
+        );
       newCorrectGuesses.push(...specialChars);
     } else {
       newRemainingAttempts -= 1;
     }
-  
+
     setGuess('');
     setRemainingAttempts(newRemainingAttempts);
     setCorrectGuesses(newCorrectGuesses);
-  
+
     if (
       newRemainingAttempts === 0 ||
       wordToGuess.word
@@ -125,15 +138,21 @@ const App = () => {
     ) {
       setGameOver(true);
     }
-    
   };
-  
-  const isLetterGuessed = (letter: string) => correctGuesses.map((l) => l.toLowerCase()).includes(letter.toLowerCase());
+
+  const isLetterGuessed = (letter: string) =>
+    correctGuesses.map((l) => l.toLowerCase()).includes(letter.toLowerCase());
 
   const getHiddenWord = () =>
     wordToGuess.word
       .split('')
-      .map((letter) => (letter === ' ' ? ' ' : isLetterGuessed(letter) || isLetterGuessed(removeDiacritics(letter)) ? letter : '_'))
+      .map((letter) =>
+        letter === ' '
+          ? ' '
+          : isLetterGuessed(letter) || isLetterGuessed(removeDiacritics(letter))
+          ? letter
+          : '_'
+      )
       .join(' ');
 
   const restartGame = () => {
@@ -145,129 +164,232 @@ const App = () => {
   };
 
   return (
-    <Center h='100vh' bg='gray.50' background='linear-gradient(0deg, rgba(255,255,255,1) 0%, rgba(241,227,255,1) 100%)'>
-    <Stack textAlign="center"  spacing={4} width={{ base: '90%', sm: '80%', md: '50%', lg: '30%', xl: '20%'}}>
-      <Heading lineHeight='tall' color='gray.600'>
-        <Highlight
-          query='CACO'
-          styles={{ px: '2', py: '1', rounded: 'full', bg: 'green.200', color: 'white' }}
-        >
-          PsiCACOlogia
-        </Highlight>
-      </Heading>
+    <Center
+      h='100vh'
+      bg='gray.50'
+      background='linear-gradient(0deg, rgba(255,255,255,1) 0%, rgba(241,227,255,1) 100%)'>
+      <Stack
+        textAlign='center'
+        spacing={4}
+        width={{ base: '90%', sm: '80%', md: '50%', lg: '30%', xl: '20%' }}>
+        <Heading
+          lineHeight='tall'
+          color='gray.600'>
+          <Highlight
+            query='CACO'
+            styles={{ px: '2', py: '1', rounded: 'full', bg: 'green.200', color: 'white' }}>
+            PsiCACOlogia
+          </Highlight>
+        </Heading>
 
-      <Center>
-        {gameOver && remainingAttempts === 0 ? (
-          <Image src={caco5} alt="kermit" {...imageProps} />
-        ) : gameOver && remainingAttempts > 0 ? (
-          <Image src={caco3} alt="kermit" {...imageProps} />
-        ) : !gameOver && remainingAttempts > 2 && remainingAttempts <= 4 ? (
-          <Image src={caco6} alt="kermit" {...imageProps} />
-        ) : !gameOver && remainingAttempts === 2 ? (
-          <Image src={caco4} alt="kermit" {...imageProps} />
-        ) : !gameOver && remainingAttempts === 1 ? (
-          <Image src={caco7} alt="kermit" {...imageProps} />
-        ) : (
-          <Image src={caco1} alt="kermit" {...imageProps} />
-        )}
-      </Center>
+        <Center>
+          {gameOver && remainingAttempts === 0 ? (
+            <Image
+              src={caco5}
+              alt='kermit'
+              {...imageProps}
+            />
+          ) : gameOver && remainingAttempts > 0 ? (
+            <Image
+              src={caco3}
+              alt='kermit'
+              {...imageProps}
+            />
+          ) : !gameOver && remainingAttempts > 2 && remainingAttempts <= 4 ? (
+            <Image
+              src={caco6}
+              alt='kermit'
+              {...imageProps}
+            />
+          ) : !gameOver && remainingAttempts === 2 ? (
+            <Image
+              src={caco4}
+              alt='kermit'
+              {...imageProps}
+            />
+          ) : !gameOver && remainingAttempts === 1 ? (
+            <Image
+              src={caco7}
+              alt='kermit'
+              {...imageProps}
+            />
+          ) : (
+            <Image
+              src={caco1}
+              alt='kermit'
+              {...imageProps}
+            />
+          )}
+        </Center>
 
-      <Flex  gap="3" alignItems='center' justify='center'>
-        {[...Array(numberOfAttempts)].map((_, i) => (
-          i < remainingAttempts ? 
-          <Heart key={i} color="red" />
-          : <Heart key={i} color="gray" />
-        ))}
-      </Flex>
-      
-      {!gameOver ? (
-        <>
-          <Flex align='center' justify='center' gap={1}>
-            <Heading as='h5' size='xs' color='gray.500'>
-              Dica da
+        <Flex
+          gap='3'
+          alignItems='center'
+          justify='center'>
+          {[...Array(numberOfAttempts)].map((_, i) =>
+            i < remainingAttempts ? (
+              <Heart
+                key={i}
+                color='red'
+              />
+            ) : (
+              <Heart
+                key={i}
+                color='gray'
+              />
+            )
+          )}
+        </Flex>
+
+        {!gameOver ? (
+          <>
+            <Flex
+              align='center'
+              justify='center'
+              gap={1}>
+              <Heading
+                as='h5'
+                size='xs'
+                color='gray.500'>
+                Dica da
+              </Heading>
+
+              <a
+                href='https://www.instagram.com/ingraapsi/?igshid=MzRlODBiNWFlZA%3D%3D'
+                target='_blank'
+                rel='noopener noreferrer'>
+                <Container
+                  mx={0}
+                  maxW='fit-content'
+                  display='flex'
+                  alignItems='center'
+                  gap={1}
+                  paddingTop={1}
+                  paddingBottom={1}
+                  paddingLeft={2}
+                  paddingRight={2}
+                  cursor={'pointer'}
+                  borderRadius='full'
+                  bg='pink.200'
+                  _hover={{ bg: 'pink.300' }}
+                  transition={'.2s'}
+                  color='white'>
+                  <Avatar
+                    size='xs'
+                    name='ingra'
+                    src={ingra}
+                  />
+                  <Heading
+                    as='h5'
+                    size='xs'
+                    color='white'>
+                    Ingra
+                  </Heading>
+                </Container>
+              </a>
+            </Flex>
+            <Text
+              style={{ marginTop: '5px', marginBottom: '14px' }}
+              fontSize='sm'
+              as='em'
+              color='gray.500'>{`${wordToGuess.tip}`}</Text>
+
+            <Heading
+              lineHeight='tall'
+              as='h4'
+              size='md'>
+              <Highlight
+                query={getHiddenWord()}
+                styles={{
+                  px: '4',
+                  py: '2',
+                  rounded: 'full',
+                  bg: 'purple.300',
+                  color: 'white',
+                  textTransform: 'uppercase',
+                }}>
+                {`${getHiddenWord()}`}
+              </Highlight>
             </Heading>
-              
-            <a href="https://www.instagram.com/ingraapsi/?igshid=MzRlODBiNWFlZA%3D%3D" target="_blank" rel="noopener noreferrer">
-              <Container mx={0} maxW='fit-content' display='flex' alignItems='center' gap={1} paddingTop={1} paddingBottom={1} paddingLeft={2} paddingRight={2} cursor={'pointer'} borderRadius='full' bg='pink.200'  _hover={{ bg: "pink.300" }} transition={'.2s'} color='white'>
-                <Avatar size='xs' name='ingra' src={ingra} />
-                <Heading as='h5' size='xs' color='white'>
-                  Ingra
-                </Heading>
-              </Container>
-            </a>
 
-          
-          </Flex>
-          <Text style={{marginTop: '5px', marginBottom: '14px'}} fontSize='sm' as='em' color='gray.500'>{`${wordToGuess.tip}`}</Text>
-          
+            <Input
+              placeholder='Letra'
+              value={guess}
+              onChange={handleGuessChange}
+              onKeyDown={(e) => {
+                if (e.key === 'Enter') {
+                  handleGuessSubmit();
+                }
+              }}
+              variant='filled'
+              size='md'
+              marginY={2}
+            />
 
-          <Heading lineHeight='tall' as='h4' size='md'>
-            <Highlight
-              query={getHiddenWord()}
-              styles={{ px: '4', py: '2', rounded: 'full', bg: 'purple.300', color: 'white', textTransform: 'uppercase' }}
-            >
-              {`${getHiddenWord()}`}
-            </Highlight>
-          </Heading>
+            <Button
+              leftIcon={<FaPen />}
+              colorScheme='blue'
+              onClick={handleGuessSubmit}>
+              Preencher Letra
+            </Button>
+          </>
+        ) : (
+          <>
+            <Heading
+              as='h5'
+              size='md'
+              color={remainingAttempts === 0 ? 'red.400' : 'green.400'}>
+              {remainingAttempts === 0 ? 'Vixe. Errou...' : 'Aí se garante!'}
+            </Heading>
 
-          <Input
-            placeholder="Letra"
-            value={guess}
-            onChange={handleGuessChange}
-            onKeyDown={(e) => {
-              if (e.key === 'Enter') {
-                handleGuessSubmit();
-              }
-            }}
-            variant='filled'
-            size="md"
-            marginY={2}
-          />
+            <Heading
+              as='h5'
+              size='xs'
+              color='gray.500'
+              style={{ marginTop: '31px' }}>
+              A resposta era:
+            </Heading>
+            <Heading
+              lineHeight='tall'
+              as='h4'
+              size='md'>
+              <Highlight
+                query={wordToGuess.word}
+                styles={{
+                  px: '4',
+                  py: '2',
+                  rounded: 'full',
+                  bg: remainingAttempts === 0 ? 'red.400' : 'green.400',
+                  color: 'white',
+                  textTransform: 'uppercase',
+                }}>
+                {`${wordToGuess.word}`}
+              </Highlight>
+            </Heading>
+            <Input
+              placeholder='Letra'
+              value={guess}
+              onChange={handleGuessChange}
+              onKeyDown={(e) => {
+                if (e.key === 'Enter') {
+                  handleGuessSubmit();
+                }
+              }}
+              variant='filled'
+              size='md'
+              marginY={2}
+              disabled
+            />
 
-          <Button leftIcon={<FaPen />} colorScheme='linkedin' onClick={handleGuessSubmit}>
-            Preencher Letra
-          </Button>
-          
-        </>
-      ) : (
-        <>
-
-          <Heading as='h5' size='md' color={remainingAttempts === 0 ? 'red.400' : 'green.400'}>
-          {remainingAttempts === 0 ? 'Vixe. Errou...' : 'Aí se garante!'}
-          </Heading>
-          
-          <Heading as='h5' size='xs' color='gray.500' style={{marginTop: '30px'}}>
-            A palavra era:
-          </Heading>
-          <Heading  lineHeight='tall' as='h4' size='md'>
-            <Highlight
-              query={wordToGuess.word}
-              styles={{ px: '4', py: '2', rounded: 'full', bg: remainingAttempts === 0 ? 'red.400' : 'green.400', color: 'white', textTransform: 'uppercase' }}
-            >
-              {`${wordToGuess.word}`}
-            </Highlight>
-          </Heading>
-          <Input
-            placeholder="Letra"
-            value={guess}
-            onChange={handleGuessChange}
-            onKeyDown={(e) => {
-              if (e.key === 'Enter') {
-                handleGuessSubmit();
-              }
-            }}
-            variant='filled'
-            size="md"
-            marginY={2}
-            disabled
-          />
-          
-          <Button leftIcon={<FaRedoAlt />} colorScheme='linkedin' onClick={restartGame} mt={4}>
-            Jogar Novamente
-          </Button>
-        </>
-      )}
-    </Stack>
+            <Button
+              leftIcon={<FaRedoAlt />}
+              colorScheme='blue'
+              onClick={restartGame}>
+              Jogar Novamente
+            </Button>
+          </>
+        )}
+      </Stack>
     </Center>
   );
 };
